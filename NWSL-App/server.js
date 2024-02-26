4d631aee57b3bbb381eb6f344359d9fd5b8d24a0
julibennett <julibennett4@gmail.com>
2024-02-25 17:24:40 -0800

    env set up and basic server.js setup

---
+++ NWSL-App/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+require('./config/database')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.json()) // express.json() allows our server to accept json 
+app.use(express.urlencoded({extended: true})) // express.urlencoded({extended:true})
+app.use(methodOverride('_method')) // allows us to override forms POST request.
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.listen(3000, () => {console.log('The server is tracking womens soccer on 3000 !!!')})
