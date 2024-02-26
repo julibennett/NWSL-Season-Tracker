3e65b0e79bb3a0ee271f310fd70d9948601ea420
julibennett <julibennett4@gmail.com>
2024-02-25 19:23:16 -0800

    userController set up

---
+++ NWSL-App/controllers/userController.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs' {currentUser: req.session.currentUser})
+})
+
+router.post('/', async(req, res) => {
+    try{
+        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+        const newUser = await User.create(req.body)
+        console.log(newUser, 'THIS IS A NEW USER')
+        res.redirect('/')
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+module.exports = router
