79526ff3e4c3985cf2b93c35812904a6da72b858
julibennett <julibennett4@gmail.com>
2024-02-25 17:52:02 -0800

    database.js set up

---
+++ NWSL-App/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI
+
+//connect to mongo 
+mongoose.connect(mongoURI)
+
+//create messages to check mongo connection
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + 'error with mongo connection'))
+db.on('connected', () => console.log('mongo is connected'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
