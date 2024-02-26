ea02350e75661a80c9ab6bae9080758a81772f3a
julibennett <julibennett4@gmail.com>
2024-02-25 21:20:26 -0800

    debugging log in page

--- NWSL-App/controllers/isAuthenticated.js
+++ NWSL-App/controllers/isAuthenticated.js
@@ -1,6 +1,7 @@
 const isAuthenticated = (req, res, next) => {
     if (req.session.currentUser) {
         console.log(req.session.currentUser)
+        return next()
     } else {
         console.log(req.session.currentUser)
         res.redirect('/sessions/new')
