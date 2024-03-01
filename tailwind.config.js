a962b425fbafd4d2bdc547c30ac79096c9a0bc41
julibennett <julibennett4@gmail.com>
2024-02-29 21:12:07 -0800

    i feel like some tailwind is working like text-center but nothing else?

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  colors: {
+    'red-500': '#EF4444', // You can replace #EF4444 with your desired color value
+      },
+  
   plugins: [],
 };
