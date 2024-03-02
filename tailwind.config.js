63405bff9dadc473ef119b83c9312884bfcbcf6e
julibennett <julibennett4@gmail.com>
2024-03-01 19:08:43 -0800

    tailwind is working thanks to billie woo!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.ejs"], 
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./views/**/*.ejs"], 
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
